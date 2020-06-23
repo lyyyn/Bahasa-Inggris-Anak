3ea2ae1dde5ba4ec0c1050940dc39722a4f18481
Linawaty <linawatylim@gmail.com>
2020-06-24 00:39:23 +0800

    BE Add guru completed

--- controllers/controllerBE.js
+++ controllers/controllerBE.js
@@ -1,43 +1,60 @@
 const guruRepo = require('../repositories/guruRepo');
-const path = require('path');
+const DASHBOARD_TEXT_COLOR = 'red-text text-darken-4';
+const DASHBOARD_SECTION_COLOR = 'red darken-4';
+const GURU_SECTION_NAME = 'Guru';
+const GURU_TEXT_COLOR = 'orange-text text-darken-3';
+const GURU_SECTION_COLOR = 'orange darken-3';
 
 module.exports = {
     adminPanel: (req, res) => {
         res.render('be/dashboard.ejs', {
             sectionName: 'Dashboard',
-            textColor: 'red-text text-darken-4',
-            sectionColor: 'red darken-4'
+            textColor: DASHBOARD_TEXT_COLOR,
+            sectionColor: DASHBOARD_SECTION_COLOR
         });
     },
     async guruList(req, res) {
         const items = await guruRepo.getAll();
         return res.render('be/admListing.ejs', {
             items,
-            sectionName: 'Guru',
-            textColor: 'orange-text text-darken-3',
-            sectionColor: 'orange darken-3'
+            sectionName: GURU_SECTION_NAME,
+            textColor: GURU_TEXT_COLOR,
+            sectionColor: GURU_SECTION_COLOR
         });
     },
     guruAdd: (req, res) => {
         res.render('be/edit.ejs',
             {
-                sectionName: 'Guru',
-                type: 'Add'
+                sectionName: GURU_SECTION_NAME,
+                type: 'Add',
+                textColor: GURU_TEXT_COLOR,
+                sectionColor: GURU_SECTION_COLOR
             });
     },
     guruEdit: (req, res) => {
         res.render('be/edit.ejs',
             {
-                sectionName: 'Guru',
+                sectionName: GURU_SECTION_NAME,
                 type: 'Edit',
-                idx: '/' + req.params.idx
+                idx: '/' + req.params.idx,
+                textColor: GURU_TEXT_COLOR,
+                sectionColor: GURU_SECTION_COLOR
             });
     },
-    guruCreate: (req, res) => {
+    async guruCreate (req, res) {
         // add to database
-        // guru.push(req.body);
-        // res.redirect('/guruList');
-        res.send('guru Added');
+        const item = {
+            'Title': req.body.judul,
+            'Artikel': req.body.artikel,
+            'Status': (req.body.status === 'on'? true : false),
+            'CreatedDate': new Date(Date.now()).toISOString(),
+            'CreatedBy': 'Lina',
+            'UpdatedDate': '',
+            'UpdatedBy': ''
+        };
+        await guruRepo.create(item);
+        // return res.send(item); //debug
+        res.redirect('/kntrblkg/guru');
     },
     guruUpdate: (req, res) => {
         // add our form data to our Array Data
